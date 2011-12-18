0c05d25d6de75214035d30fac61cf64b5eee60fa
Sir Valiance <sir@sirvaliance.com>
2011-12-17 20:22:10 -0600

    Updated to have compojure as the framwork

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject sirvaliance "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-jetty-adapter "0.3.9"]])
+                 [ring/ring-jetty-adapter "0.3.9"]
+                 [compojure "0.6.4"]])
