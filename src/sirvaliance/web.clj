fec9307b757fbb722076fac8331da64a98d1963d
Sir Valiance <sir@sirvaliance.com>
2011-12-17 19:22:11 -0600

    Added initial project files.

---
+++ src/sirvaliance/web.clj
@@ -0,0 +1,12 @@
+(ns sirvaliance.web
+  (:use ring.adapter.jetty))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world"})
+
+(defn -main []
+  (let [port (Integer/parseInt (System/getenv "PORT"))]
+    (run-jetty app {:port port})))
+
---
+++ test/sirvaliance/test/core.clj
@@ -0,0 +1,6 @@
+(ns sirvaliance.test.core
+  (:use [sirvaliance.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
